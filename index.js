eef22e7feb3a09738739a7b2c731d4dc47f964e9
Aparna <aparna.pa@v2solutions.com>
2024-07-10 20:02:31 +0530

    Logic Implemented

---
+++ index.js
@@ -0,0 +1,68 @@
+
+// Accessing Control Buttons using DOM
+document.getElementById("startPause").addEventListener("click", startPause);
+document.getElementById("stop").addEventListener("click", stop);
+document.getElementById("reset").addEventListener("click", reset);
+
+// Initializing  variables of Timer
+let timer;
+let isRunning = false;
+let clearTimer = false;
+let seconds = 0, minutes = 0, hours = 0;
+
+// Control Button Logic (StartPause, Stop and Reset)
+
+function startPause() {
+    // Logic for Pause Control Button
+    if (isRunning) {
+        clearInterval(timer);
+    }
+    // Logic for Resetting Values after Stop Button is clicked 
+    else if (clearTimer && !isRunning) {
+        seconds = -1;
+        minutes = 0;
+        hours = 0;
+        updateDisplay();
+        timer = setInterval(updateDisplay, 500);
+    }
+    // Logic for Start Control Button
+    else {
+        timer = setInterval(updateDisplay, 500);
+    }
+    isRunning = !isRunning;
+}
+
+function stop() {
+    clearInterval(timer);
+    isRunning = false;
+    clearTimer = true;
+    console.log(clearTimer);
+}
+
+function reset() {
+    clearInterval(timer);
+    isRunning = false;
+    seconds = -1;
+    minutes = 0;
+    hours = 0;
+    updateDisplay();
+}
+
+// Update Timer Display
+function updateDisplay() {
+    seconds++;
+    if (seconds >= 60) {
+        seconds = 0;
+        minutes++;
+        if (minutes >= 60) {
+            minutes = 0;
+            hours++;
+        }
+    }
+    let displayString = `${calcTime(hours)}:${calcTime(minutes)}:${calcTime(seconds)}`;
+    document.getElementById("displayTime").textContent = displayString;
+}
+
+function calcTime(number) {
+    return (number < 10) ? "0" + number : number;
+}
